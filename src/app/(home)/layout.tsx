0eafeeb3fe43d30cdbc29e26b52addb78659667b
PYantian <1779074976@qq.com>
2026-03-28 15:24:39 +0800

    Comment out HomeLayout function in layout.tsx
    Comment out the HomeLayout function and its imports.

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,18 +1,18 @@
-import type { ReactNode } from 'react'
-import Layout from '@/layout'
-import { auth } from '@/lib/auth'
-import { redirect } from 'next/navigation'
+// import type { ReactNode } from 'react'
+// import Layout from '@/layout'
+// import { auth } from '@/lib/auth'
+// import { redirect } from 'next/navigation'
 
-export default async function HomeLayout({
-	children
-}: {
-	children: ReactNode
-}) {
-	const session = await auth()
+// export default async function HomeLayout({
+// 	children
+// }: {
+// 	children: ReactNode
+// }) {
+// 	const session = await auth()
 
-	if (!session?.user) {
-		redirect('/sign-in')
-	}
+// 	if (!session?.user) {
+// 		redirect('/sign-in')
+// 	}
 
-	return <Layout>{children}</Layout>
-}
+// 	return <Layout>{children}</Layout>
+// }
