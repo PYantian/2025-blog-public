333a6faa2f15752822286f898915f8fc8b22e562
PYantian <1779074976@qq.com>
2026-03-30 12:29:03 +0800

    Refactor ArtCard component layout and styles

--- src/app/(main)/(home)/art-card.tsx
+++ src/app/(main)/(home)/art-card.tsx
@@ -1,41 +1,52 @@
 import Card from '@/components/card'
 import { useCenterStore } from '@/hooks/use-center'
 import { useConfigStore } from './stores/config-store'
-import { CARD_SPACING } from '@/consts'
 import { useRouter } from 'next/navigation'
 import { HomeDraggableLayer } from './home-draggable-layer'
+import { useHomeLayoutMode } from './utils/home-layout-mode'
+import { resolveHomeCardFrame } from './utils/resolve-home-card-frame'
 
 export default function ArtCard() {
-	const center = useCenterStore()
-	const { cardStyles, siteContent } = useConfigStore()
-	const router = useRouter()
-	const styles = cardStyles.artCard
-	const hiCardStyles = cardStyles.hiCard
+  const center = useCenterStore()
+  const { cardStyles, siteContent } = useConfigStore()
+  const router = useRouter()
+  const mode = useHomeLayoutMode()
 
-	const x = styles.offsetX !== null ? center.x + styles.offsetX : center.x - styles.width / 2
-	const y = styles.offsetY !== null ? center.y + styles.offsetY : center.y - hiCardStyles.height / 2 - styles.height - CARD_SPACING
+  const styles = resolveHomeCardFrame(cardStyles, 'artCard', mode)
 
-	const artImages = siteContent.artImages ?? []
-	const currentId = siteContent.currentArtImageId
-	const currentArt = (currentId ? artImages.find(item => item.id === currentId) : undefined) ?? artImages[0]
-	const artUrl = currentArt?.url || '/images/art/cat.png'
+  const x = center.x + (styles.offsetX ?? -(styles.width / 2))
+  const y = center.y + (styles.offsetY ?? -(styles.height / 2))
 
-	return (
-		<HomeDraggableLayer cardKey='artCard' x={x} y={y} width={styles.width} height={styles.height}>
-			<Card className='p-2 max-sm:static max-sm:translate-0' order={styles.order} width={styles.width} height={styles.height} x={x} y={y}>
-				{siteContent.enableChristmas && (
-					<>
-						<img
-							src='/images/christmas/snow-3.webp'
-							alt='Christmas decoration'
-							className='pointer-events-none absolute'
-							style={{ width: 160, right: -8, top: -16, opacity: 0.9 }}
-						/>
-					</>
-				)}
+  const artImages = siteContent.artImages ?? []
+  const currentId = siteContent.currentArtImageId
+  const currentArt =
+    (currentId ? artImages.find(item => item.id === currentId) : undefined) ?? artImages[0]
+  const artUrl = currentArt?.url || '/images/art/cat.png'
 
-				<img onClick={() => router.push('/pictures')} src={artUrl} alt='wall art' className='h-full w-full rounded-[32px] object-cover' />
-			</Card>
-		</HomeDraggableLayer>
-	)
+  return (
+    <HomeDraggableLayer
+      cardKey='artCard'
+      defaultX={x}
+      defaultY={y}
+      width={styles.width}
+      height={styles.height}
+    >
+      <Card x={x} y={y} width={styles.width} height={styles.height} order={styles.order}>
+        {siteContent.enableChristmas && (
+          <img
+            src='/images/christmas-1.webp'
+            alt='Christmas decoration'
+            className='pointer-events-none absolute -top-10 -left-6 z-10 size-24 -rotate-12 select-none'
+          />
+        )}
+
+        <img
+          onClick={() => router.push('/pictures')}
+          src={artUrl}
+          alt='wall art'
+          className='h-full w-full rounded-[32px] object-cover'
+        />
+      </Card>
+    </HomeDraggableLayer>
+  )
 }
